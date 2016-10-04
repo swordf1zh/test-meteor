e25b7ea5eea15efe66b3cda273faf6dc57036ad3
rtribaldos <rtribaldos@registro-publico.gob.pa>
2016-10-04 16:17:28 -0500

    Step 4 - Authentication (with password)

--- client/scripts/controllers/login.controller.js
+++ client/scripts/controllers/login.controller.js
@@ -1,33 +1,22 @@
 import { _ } from 'meteor/underscore';
-import { Accounts } from 'meteor/accounts-base';
+import { Accounts } from 'meteor/accounts-base'
 import { Controller } from 'angular-ecmascript/module-helpers';
 
 export default class LoginCtrl extends Controller {
   login() {
-    if (_.isEmpty(this.phone)) return;
+    if (_.isEmpty(this.user) || _.isEmpty(this.pass)) return;
 
-    const confirmPopup = this.$ionicPopup.confirm({
-      title: 'Number confirmation',
-      template: '<div>' + this.phone + '</div><div>Is your phone number above correct?</div>',
-      cssClass: 'text-center',
-      okText: 'Yes',
-      okType: 'button-positive button-clear',
-      cancelText: 'edit',
-      cancelType: 'button-dark button-clear'
+    this.$ionicLoading.show({
+      template: 'Verifying user data...'
     });
 
-    confirmPopup.then((res) => {
-      if (!res) return;
-
-      this.$ionicLoading.show({
-        template: 'Sending verification code...'
-      });
-
-      Accounts.requestPhoneVerification(this.phone, (err) => {
-        this.$ionicLoading.hide();
-        if (err) return this.handleError(err);
-        this.$state.go('confirmation', { phone: this.phone });
-      });
+    Accounts.createUser({
+      username: this.user,
+      password: this.pass
+    }, (err) => {
+      this.$ionicLoading.hide();
+      if (err) return this.handleError(err);
+      this.$state.go('profile', { username: this.user });
     });
   }
 
